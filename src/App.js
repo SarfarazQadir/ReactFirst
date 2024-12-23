9f279a2995026f01d28272f266ba1096f9927edc
Sarfaraz Qadir <sarfarazqadir594@gmail.com>
2024-12-23 21:52:38 +0500

    First

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import { Login, Signup } from './Screens';
+
+function App() {
+  return (
+    <>
+      <Signup/>
+    </>
+  );
+}
+
+export default App;
